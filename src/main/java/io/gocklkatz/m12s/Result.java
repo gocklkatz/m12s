6f24c0202f70cdaa206186865286576b40cc9831
Stefan Katzensteiner <stefan.katzensteiner@mailbox.org>
2024-07-11 07:46:59 +0200

    Renaming and comments

--- src/main/java/io/gocklkatz/m12s/Result.java
+++ src/main/java/io/gocklkatz/m12s/Result.java
@@ -3,14 +3,33 @@ package io.gocklkatz.m12s;
 import java.util.List;
 import java.util.Objects;
 
+/*
+    Objective function (Zielfunktion)
+
+    The function f is called, variously, an objective function, criterion function a loss function or
+    cost function (minimization),[6] a utility function or fitness function (maximization), or, in
+    certain fields, an energy function or energy functional.
+
+    A feasible solution that minimizes (or maximizes, if that is the goal) the objective function is
+    called an optimal solution.
+
+    ---
+    Q/A
+    - Why do I need heuristics/meta-heurstics? -> x
+    - Is assignment problem (AP) an x?
+    - Solution methods for the AP. (Hungarian)
+      # Brute force. n! permutations
+      # Greedy algorithm (heuristic)
+      # Hungarian method (?)
+ */
 public class Result<T> {
 
     private final List<T> solutionVector;
-    private final T targetFunctionValue;
+    private final T objectiveFunctionValue;
 
-    public Result(List<T> solutionVector, T targetFunctionValue) {
+    public Result(List<T> solutionVector, T objectiveFunctionValue) {
         this.solutionVector = solutionVector;
-        this.targetFunctionValue = targetFunctionValue;
+        this.objectiveFunctionValue = objectiveFunctionValue;
     }
 
     public List<T> getSolutionVector() {
@@ -18,7 +37,7 @@ public class Result<T> {
     }
 
     public T getTargetFunctionValue() {
-        return targetFunctionValue;
+        return objectiveFunctionValue;
     }
 
     @Override
@@ -27,13 +46,13 @@ public class Result<T> {
         if (o == null || getClass() != o.getClass()) return false;
 
         Result<?> result = (Result<?>) o;
-        return Objects.equals(solutionVector, result.solutionVector) && Objects.equals(targetFunctionValue, result.targetFunctionValue);
+        return Objects.equals(solutionVector, result.solutionVector) && Objects.equals(objectiveFunctionValue, result.objectiveFunctionValue);
     }
 
     @Override
     public int hashCode() {
         int result = Objects.hashCode(solutionVector);
-        result = 31 * result + Objects.hashCode(targetFunctionValue);
+        result = 31 * result + Objects.hashCode(objectiveFunctionValue);
         return result;
     }
 
@@ -41,7 +60,7 @@ public class Result<T> {
     public String toString() {
         return "Solution{" +
                 "solutionVector=" + solutionVector +
-                ", targetFunctionValue=" + targetFunctionValue +
+                ", targetFunctionValue=" + objectiveFunctionValue +
                 '}';
     }
 }
